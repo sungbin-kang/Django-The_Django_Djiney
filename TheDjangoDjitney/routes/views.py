3e1c6f3ad29ac6c4043d391dbcab6fb6cc2ac593
Sungbin_Kang <sungbin9353@gmail.com>
2021-11-15 22:28:31 -0800

    routes models, urls, views edited

--- TheDjangoDjitney/routes/views.py
+++ TheDjangoDjitney/routes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Line, Station, Stop
+from .forms import  StopForm, LineForm, StationForm
+# Add your imports below:
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomeView(TemplateView):
+  template_name = "routes/home.html"
+
+  def get_context_data(self):
+    context = super().get_context_data()
+    context["lines"] = Line.objects.all()
+    context["stations"] = Station.objects.all()
+    context["stops"] = Stop.objects.all()
+    return context
