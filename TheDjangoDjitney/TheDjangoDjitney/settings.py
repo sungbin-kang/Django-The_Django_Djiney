0d535f940f9a582b1bd006dd265d4ddbb5684797
Sungbin_Kang <sungbin9353@gmail.com>
2021-11-15 22:05:35 -0800

    Sensitive data removed

--- TheDjangoDjitney/TheDjangoDjitney/settings.py
+++ TheDjangoDjitney/TheDjangoDjitney/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%89*0f(dh9=(+f-8)dhq^3w+1zvk1lf0b%1sr$sc9nh_5!451e'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
